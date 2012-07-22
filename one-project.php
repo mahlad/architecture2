e8346cbeec0dab1a1f1517abf085374b7c2a45eb
mahlad <mahlad365@gmail.com>
2012-07-23 00:36:19 +0430

    show just one project with the right nav(projects list)

---
+++ one-project.php
@@ -0,0 +1,18 @@
+<?php
+	include'header.php';
+?>
+<div class="w5 right">
+	<nav class="project">
+    	<ul>
+        	<li><a href="">پروژه شماره 111</a></li>
+            <li><a href="">پروژه شماره 112</a></li>
+            <li><a href="">پروژه شماره 113</a></li>
+            <li><a href="">پروژه شماره 114</a></li>
+        </ul>
+    </nav>
+</div>
+<article class="last mr6" ></article>
+
+<?php
+	include'footer.php';
+?>
