cdbea87aa318e829c70e72cee954c22e4ca08e5d
mahlad <mahlad365@gmail.com>
2012-07-22 14:41:40 +0430

    change to php site for creat other pages

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+	include'header.php';
+?>
+        	<article class="first">
+            	<section></section>
+            </article>
+            <article class="second">
+            	<section>
+                	<p>Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                    <p>Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                </section>
+            </article>
+            <article class="third">
+            	<section class="w6 right">
+                	<img src="images/image1-190x111.jpg">
+                    <h4>نام یا موضوع پروژه</h4>
+                    <p class="w5 right">توضیحات:
+                     gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.
+                    Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                    <a href="" class="right">ادامه مطلب</a>
+                </section>
+                <section class="w6 right">
+                	<img src="images/image1-190x111.jpg">
+                    <h4>نام یا موضوع پروژه</h4>
+                    <p class="w5  right">توضیحات:
+                     gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.
+                    Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                    <a href="" class="right">ادامه مطلب</a>
+                </section>
+                <section class="w6 right">
+                	<img src="images/image1-190x111.jpg" width="190px" height="111px">
+                    <h4>نام یا موضوع پروژه</h4>
+                    <p class="w5  right">توضیحات:
+                     gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.
+                    Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                    <a href="" class="right">ادامه مطلب</a>
+                </section>
+                <section class="w6 right">
+                	<img src="images/image1-190x111.jpg">
+                    <h4>نام یا موضوع پروژه</h4>
+                    <p class="w5 right">توضیحات:
+                     gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.
+                    Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                    <a href="" class="right">ادامه مطلب</a>
+                </section>
+            </article>
+            <article class="forth">
+            	<section>
+                	<p>Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.
+                    Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus. Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. Morbi ac commodo nulla.</p>
+                </section>
+            </article>
+
+<?php
+	include'footer.php';
+?>
