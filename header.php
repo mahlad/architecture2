042162d2ea0a204f35fb2be17cc9b2f4fe71104b
mahlad <mahlad365@gmail.com>
2012-07-23 16:12:29 +0430

    seprate the head(link) from the header beacuse of css in index page

--- header.php
+++ header.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-<meta charset="utf-8" />
-<title>Architecture</title>
-	<link rel="stylesheet" href="css/reset.css" >
-    <link rel="stylesheet" href="css/grid.css" >
-    <link rel="stylesheet" href="style.css" >
-    <link rel="stylesheet" href="architecture2-pages.css">
-    <script src="js/html5shiv.js"></script>
-</head>
+<?php
+	include'head.php';
+?>
 
 <body>
 	<header>
