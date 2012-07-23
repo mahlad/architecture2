c5e3c818f83d7cf44bd6279332d7f442d31d8e7f
mahlad <mahlad365@gmail.com>
2012-07-23 16:13:05 +0430

    design the welcom page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	include'head.php';
+?>
+<div class="welcom" id="bg-welcom">
+	<div class="bg-pattern">
+    	<ul>
+        	<li><a href="home.php">صفحه اصلی</a></li>
+            <li><a href="projects.php">پروژه ها</a></li>
+            <li><a href="">استخدام</a></li>
+                     
+        </ul>
+    </div>
+</div>
